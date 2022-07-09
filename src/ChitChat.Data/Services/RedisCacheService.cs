f5f7f105932789b25ac4de8c11d149875f6de407
Abdullah-Asif <asifabdullah135@gmail.com>
2022-07-09 21:23:34 +0600

    add pattern for getting the key with prefix

--- src/ChitChat.Data/Services/RedisCacheService.cs
+++ src/ChitChat.Data/Services/RedisCacheService.cs
@@ -12,12 +12,17 @@ public class RedisCacheService : ICacheService
 
     public List<string> GetAllKeysAsync()
     {
-       
-        List<string> keyList = new List<string>();
-        var keys =  _connectionMultiplexer.GetServer("localhost", 6379).Keys();
-            keyList.AddRange(keys.Select(key => (string)key).ToList());
- 
-        return keyList;
+        var pattern = "DIU_Raizor";
+
+        var keys = _connectionMultiplexer
+            .GetServer("localhost", 6379).Keys(0, pattern + "*").ToList();
+        
+        var strings = string.Join("", keys);
+        
+        var list = strings.Split("DIU_Raizor:").ToList();
+        list.RemoveAt(0);
+
+        return list;
     }
 
     public async Task<string> GetCachValueAsync(string key)
@@ -29,7 +34,8 @@ public class RedisCacheService : ICacheService
     public async Task SetCachValueAsync(string key, string value)
     {
         var db = _connectionMultiplexer.GetDatabase();
-        await db.StringSetAsync(key, value);
-        await db.KeyExpireAsync(key, DateTime.UtcNow.AddSeconds(60));
+        var prefix = "DIU_Raizor:";
+        await db.StringSetAsync(prefix + key, value);
+        await db.KeyExpireAsync(prefix + key, DateTime.UtcNow.AddSeconds(60));
     }
 }
