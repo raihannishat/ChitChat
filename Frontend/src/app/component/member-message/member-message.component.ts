304c4dd2d0457115ff7dd3a4715a19a5b7f7ab74
Abdullah-Asif <asifabdullah135@gmail.com>
2022-08-17 22:15:20 +0600

    sender name added with receiver

--- Frontend/src/app/component/member-message/member-message.component.ts
+++ Frontend/src/app/component/member-message/member-message.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Message } from 'src/app/models/message.model';
@@ -8,39 +14,43 @@ import { MessageService } from 'src/app/services/message.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-message',
   templateUrl: './member-message.component.html',
-  styleUrls: ['./member-message.component.css']
+  styleUrls: ['./member-message.component.css'],
 })
 export class MemberMessageComponent implements OnInit {
-
   @ViewChild('messageForm') messageForm!: NgForm;
   @Input() messages!: Message[];
   username!: string | any;
   member!: string | any;
   messageContent!: string;
 
-  constructor(public messageService: MessageService, public route: ActivatedRoute) { }
+  constructor(
+    public messageService: MessageService,
+    public route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.username = localStorage.getItem('username')?.toString();
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.member = params['membername'];
       // console.log(params['membername']);
-    })
+    });
     this.messageService.createHubConnection(this.username, this.member);
   }
 
   loadMessages() {
-    this.messageService.getMessageThread(this.member).subscribe(messages => {
+    this.messageService.getMessageThread(this.member).subscribe((messages) => {
       this.messages = messages;
-    })
+    });
   }
 
   sendMessage() {
     console.log(this.username);
     console.log(this.messageContent);
-    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
-      this.messageForm.reset();
-    })
+    this.messageService
+      .sendMessage(this.username, this.member, this.messageContent)
+      .then(() => {
+        this.messageForm.reset();
+      });
   }
 
   ngOnDestroy(): void {
--- Frontend/src/app/services/message.service.ts
+++ Frontend/src/app/services/message.service.ts
@@ -4,55 +4,56 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
 import { Message } from '../models/message.model';
 import { User } from '../models/user.model';
-import {take} from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import { Group } from '../models/group.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
-
   baseUrl = 'https://localhost:7113/api/';
   hubUrl = 'https://localhost:7113/hubs/';
   private hubConnection!: HubConnection;
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   createHubConnection(user: string, otherUsername: string) {
     this.hubConnection = new HubConnectionBuilder()
-      .withUrl(this.hubUrl + 'message?user=' + otherUsername)
+      .withUrl(
+        this.hubUrl + 'message?sender=' + user + '&receiver=' + otherUsername
+      )
       // {
       //   accessTokenFactory: () => localStorage.getItem('token')!.toString()
       // })
       .withAutomaticReconnect()
-      .build()
+      .build();
 
-    this.hubConnection.start().catch(error => console.log(error));
+    this.hubConnection.start().catch((error) => console.log(error));
 
-    this.hubConnection.on('ReceiveMessageThread', messages => {
+    this.hubConnection.on('ReceiveMessageThread', (messages) => {
       this.messageThreadSource.next(messages);
-    })
+    });
 
-    this.hubConnection.on('NewMessage', message => {
-      this.messageThread$.pipe(take(1)).subscribe(messages => {
-        this.messageThreadSource.next([...messages, message])
-      })
-    })
+    this.hubConnection.on('NewMessage', (message) => {
+      this.messageThread$.pipe(take(1)).subscribe((messages) => {
+        this.messageThreadSource.next([...messages, message]);
+      });
+    });
 
     this.hubConnection.on('UpdatedGroup', (group: Group) => {
-      if (group.connections.some(x => x.username === otherUsername)) {
-        this.messageThread$.pipe(take(1)).subscribe(messages => {
-          messages.forEach(message => {
+      if (group.connections.some((x) => x.username === otherUsername)) {
+        this.messageThread$.pipe(take(1)).subscribe((messages) => {
+          messages.forEach((message) => {
             if (!message.dateRead) {
-              message.dateRead = new Date(Date.now())
+              message.dateRead = new Date(Date.now());
             }
-          })
+          });
           this.messageThreadSource.next([...messages]);
-        })
+        });
       }
-    })
+    });
   }
 
   stopHubConnection() {
@@ -62,12 +63,19 @@ export class MessageService {
   }
 
   getMessageThread(username: string) {
-    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+    return this.http.get<Message[]>(
+      this.baseUrl + 'messages/thread/' + username
+    );
   }
 
-  async sendMessage(username: string, content: string) {
-    return this.hubConnection.invoke('SendMessage', {recipientUsername: username, content})
-      .catch(error => console.log(error));
+  async sendMessage(sender: string, receiver: string, content: string) {
+    return this.hubConnection
+      .invoke('SendMessage', {
+        sender: sender,
+        receiver: receiver,
+        content: content,
+      })
+      .catch((error) => console.log(error));
   }
 
   deleteMessage(id: number) {
