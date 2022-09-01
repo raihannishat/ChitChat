ff61bbf4a61780846a28b3e4750cc2d773cf78ae
Abdullah-Asif <asifabdullah135@gmail.com>
2022-09-01 22:20:08 +0600

    chatbox repeated issue solved

--- Frontend/src/app/component/chatpage/chatpage.component.ts
+++ Frontend/src/app/component/chatpage/chatpage.component.ts
@@ -1,5 +1,7 @@
+import { Message } from 'src/app/models/message.model';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
+import { MessageService } from 'src/app/services/message.service';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -9,20 +11,34 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class ChatpageComponent implements OnInit {
   users: User[] = [];
+  messages: Message[] = [];
   user: string = 'nishat';
   userClicked: boolean = false;
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe((response) => {
       this.users = response;
+      this.user = this.users[0].name;
     });
   }
 
-  onClick() {
+  onClick(user: User) {
     this.userClicked = true;
-  }
+    console.log(user);
 
+    this.user = user.name;
+    this.loadMessages();
+  }
+  loadMessages() {
+    this.messageService.getMessageThread(this.user).subscribe((messages) => {
+      debugger;
+      this.messages = messages;
+    });
+  }
   get userClick() {
     return this.userClicked;
   }
