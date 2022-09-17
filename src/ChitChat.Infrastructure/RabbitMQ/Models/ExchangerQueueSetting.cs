37c61d951d1b3a99de8ee21aade22bf97e4d694a
Abdullah-Asif <asifabdullah135@gmail.com>
2022-09-17 16:16:17 +0600

    queue name changed

--- src/ChitChat.Infrastructure/RabbitMQ/Models/ExchangerQueueSetting.cs
+++ src/ChitChat.Infrastructure/RabbitMQ/Models/ExchangerQueueSetting.cs
@@ -3,6 +3,6 @@
 public class ExchangerQueueSetting
 {
     public string DatabaseQueue { get { return "database"; } }
-    public string SigalRQueue { get { return "signalR"; } }
+    public string SigalRQueue { get { return "signalR1"; } }
     public string Exchange { get { return "DIU_Raizor"; } }
 }
